5cde29e2a983fad6044f87c61e361183bfa14dcf
Rob Mrowicki <robert.mrowicki@gmail.com>
2020-10-13 15:08:08 +0000

    plot MDS and CAP ordinations for lichen trait CWMs *tweak plotting script

--- scripts/04_plotting.R
+++ scripts/04_plotting.R
@@ -7,7 +7,7 @@
 mds_mar <- c(0.4, 0.4, 0.4, 0.4)
 cap_mar <- c(3.5, 3.5, 0.5, 0.5)
 
-# lookup tables for point styles:
+# lookup table for point styles:
 pt_sty_site <- data.frame(  # by site
   site = levels(dd_lichens_taxa$site),
   pch = c(0, 1, 17),  # symbol
@@ -16,26 +16,12 @@ pt_sty_site <- data.frame(  # by site
   col = c(grey(0.5), "salmon", "royalblue")  # colour
 )
 
-pt_sty_girth <- data.frame(  # by girth
-  girth = levels(dd_tree_lichens_taxa$girth),
-  pch = c(3, 1, 17),  # symbol
-  cex = rep(0.8, 3),  # size
-  col = rep(grey(0.5), 3)  # colour
-)
-
-pt_sty_bark <- data.frame(  # by bark type
-  bark = levels(dd_tree_lichens_taxa$bark),
-  pch = c(4, 3, 2, 1),  # symbol
-  cex = rep(0.8, 4),  # size
-  col = rep(grey(0.5), 4)  # colour
-)
-
 
 
 
 # A. MAIN PLOTS ================================================================
 
-# 1. Boxplots of lichen diversity vs. site ('plot-level') ======================
+# 1. Boxplots of lichen diversity vs. site ('plot-level') ----------------------
 
 # create boxplots:
 
@@ -129,19 +115,22 @@ dev.off()  # close .pdf graphics device
 
 
 
-# 2. MDS ordination plots ('plot-level') =======================================
+# 2. MDS ordination plots ('plot-level') ---------------------------------------
 
 pdf(  # open .pdf graphics device
   './figs/mds_plot.pdf',
-  width = 18/2.54, height = 9/2.54
+  width = 18/2.54, height = 6/2.54
 )
 
 
 par( # set plotting parameters
   las = 1, mar = mds_mar)
 
-par(fig = c(0, 0.5, 0, 1))
-plot_mds(  # plot MDS for taxonomic groups
+
+# ~ taxonomic groups:
+
+par(fig = c(0, 1/3, 0, 1))
+plot_mds(  # plot MDS
   mds_obj = mds_li_taxa_plot,
   pt_sty_dat = pt_sty_site, pt_sty_var = "site",
   pt_ref_dat = tree_lichens_taxa_plot,
@@ -154,8 +143,10 @@ ordiellipse(  # add ellipses for site groups
 )
 
 
-par(fig = c(0.5, 1, 0, 1), new = TRUE)
-plot_mds(  # plot MDS for functional groups
+# ~ functional groups:
+
+par(fig = c(1/3, 2/3, 0, 1), new = TRUE)
+plot_mds(  # plot MDS
   mds_obj = mds_li_func_plot,
   pt_sty_dat = pt_sty_site, pt_sty_var = "site",
   pt_ref_dat = tree_lichens_func_plot,
@@ -168,12 +159,28 @@ ordiellipse(  # add ellipses for site groups
 )
 
 
+# ~ lichen traits:
+
+par(fig = c(2/3, 1, 0, 1), new = TRUE)
+plot_mds(  # plot MDS
+  mds_obj = mds_li_traits_plot,
+  pt_sty_dat = pt_sty_site, pt_sty_var = "site",
+  pt_ref_dat = tree_lichens_func_plot,
+  plot_lab = "(b)"
+)
+
+ordiellipse(  # add ellipses for site groups
+  mds_li_traits_plot, tree_lichens_func_plot$site,
+  draw = "line", col = as.character(pt_sty_site$col)
+)
+
+
 dev.off()  # close .pdf graphics device
 
 
 
 
-# CAP ordination plots ('plot-level') ==========================================
+# 3.  CAP ordination plots ('plot-level') --------------------------------------
 
 # ~~ taxonomic groups:
 
@@ -237,9 +244,40 @@ dev.off()  # close .pdf graphics device
 
 
 
+# ~~ lichen traits:
+
+pdf(  # open .pdf graphics device
+  './figs/cap_traits_plot.pdf',
+  width = 18/2.54, height = 9/2.54
+)
+
+
+par( # set plotting parameters
+  las = 1, mar = cap_mar)
+
+par(fig = c(0, 0.5, 0, 1))
+plot_cap_env(  # plot CAP ordination (with environmental correlations)
+  cap_obj = cap_traits_plot, cap_dat = tree_lichens_func_plot,
+  spp_cor_obj = cor_traits_plot_spp, env_cor_obj = cor_cap_traits_plot_env,
+  delta_sq_obj = delta_sq_traits_plot, fig_cex = 0.8,
+  pt_sty_dat = pt_sty_site, plot_lab = "(a)", leg_title = "Site"
+)
+
+par(fig = c(0.5, 1, 0, 1), new = TRUE)
+plot_cap_spp(  # plot species correlations with CAP axes
+  spp_cor_obj = cor_cap_traits_plot_spp, spp_cor_obj_lab = cor_traits_plot_spp,
+  fig_cex = 0.8, plot_lab = "(b)"
+)
+
+
+dev.off()  # close .pdf graphics device
+
+
+
+
 # B. SUPPLEMENTARY PLOTS =======================================================
 
-# 1. Boxplots of lichen diversity vs. site ('tree-level') ======================
+# 1. Boxplots of lichen diversity vs. site ('tree-level') ----------------------
 
 # create boxplots:
 
@@ -321,8 +359,8 @@ dev.off()  # close .pdf graphics device
 
 
 
-# 2. MDS ordination plots ('tree-level') =======================================
-# (NB -- MDS not converged)
+# 2. MDS ordination plots ('tree-level') ---------------------------------------
+# (NB -- MDS not converged!)
 
 pdf(  # open .pdf graphics device
   './figs/mds.pdf',
@@ -333,8 +371,11 @@ pdf(  # open .pdf graphics device
 par( # set plotting parameters
   las = 1, mar = mds_mar)
 
-par(fig = c(0, 0.5, 0, 1))
-plot_mds(  # plot MDS for taxonomic groups
+
+# ~ taxonomic groups:
+
+par(fig = c(0, 1/3, 0, 1))
+plot_mds(  # plot MDS
   mds_obj = mds_li_taxa,
   pt_sty_dat = pt_sty_site, pt_sty_var = "site",
   pt_ref_dat = dd_tree_lichens_taxa,
@@ -346,8 +387,11 @@ ordiellipse(  # add ellipses for site groups
   draw = "line", col = as.character(pt_sty_site$col)
 )
 
-par(fig = c(0.5, 1, 0, 1), new = TRUE)
-plot_mds(  # plot MDS for functional groups
+
+# ~ functional groups:
+
+par(fig = c(1/3, 2/3, 0, 1), new = TRUE)
+plot_mds(  # plot MDS
   mds_obj = mds_li_func,
   pt_sty_dat = pt_sty_site, pt_sty_var = "site",
   pt_ref_dat = dd_tree_lichens_func,
@@ -360,12 +404,28 @@ ordiellipse(  # add ellipses for site groups
 )
 
 
+# ~ lichen traits:
+
+par(fig = c(2/3, 1, 0, 1), new = TRUE)
+plot_mds(  # plot MDS
+  mds_obj = mds_li_traits,
+  pt_sty_dat = pt_sty_site, pt_sty_var = "site",
+  pt_ref_dat = dd_tree_lichens_func,
+  plot_lab = "(b)"
+)
+
+ordiellipse(  # add ellipses for site groups
+  mds_li_traits, dd_tree_lichens_func$site,
+  draw = "line", col = as.character(pt_sty_site$col)
+)
+
+
 dev.off()  # close .pdf graphics device
 
 
 
 
-# CAP ordination plots ('tree-level') ==========================================
+# 3. CAP ordination plots ('tree-level') ---------------------------------------
 
 # ~~ taxonomic groups:
 
@@ -420,6 +480,37 @@ plot_cap_env(  # plot CAP ordination (without environmental correlations)
 par(fig = c(0.5, 1, 0, 1), new = TRUE)
 plot_cap_spp(  # plot species correlations with CAP axes
   spp_cor_obj = cor_cap_func_spp, spp_cor_obj_lab = cor_func_spp,
+  fig_cex = 0.8, plot_lab = "(b)"
+)
+
+
+dev.off()  # close .pdf graphics device
+
+
+
+
+# ~~ lichen traits:
+
+pdf(  # open .pdf graphics device
+  './figs/cap_traits.pdf',
+  width = 18/2.54, height = 9/2.54
+)
+
+
+par( # set plotting parameters
+  las = 1, mar = cap_mar)
+
+par(fig = c(0, 0.5, 0, 1))
+plot_cap_env(  # plot CAP ordination (without environmental correlations)
+  cap_obj = cap_traits, cap_dat = dd_tree_lichens_func,
+  spp_cor_obj = cor_traits_spp, # env_cor_obj = cor_cap_func_env,
+  delta_sq_obj = delta_sq_traits, fig_cex = 0.8,
+  pt_sty_dat = pt_sty_site, plot_lab = "(a)", leg_title = "Site"
+)
+
+par(fig = c(0.5, 1, 0, 1), new = TRUE)
+plot_cap_spp(  # plot species correlations with CAP axes
+  spp_cor_obj = cor_cap_traits_spp, spp_cor_obj_lab = cor_traits_spp,
   fig_cex = 0.8, plot_lab = "(b)"
 )
 
